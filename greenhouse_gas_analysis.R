b6726b861bc50e835c31a4ec749bcf1fdb1bc1ee
alex-hall-data <ahall4909@gmail.com>
2018-05-08 17:23:38 +0100

    plotting improved

--- greenhouse_gas_analysis.R
+++ greenhouse_gas_analysis.R
@@ -3,6 +3,7 @@ library(dplyr)
 library(TSA)
 library(plotly)
 library(tsoutliers)
+library(zoo)
 
 df <- read.csv("raw_dataset.csv")
 
@@ -49,6 +50,9 @@ ggplot(daily_df , aes(x=indx , y=CO2))+
   labs(title="daily CO2 Data")
 
 
+
+
+
 #analyse data
 #####
 #FFT
@@ -58,17 +62,17 @@ hourly_spec <- spectrum(hourly_df$CO2 , span=50 , plot=F)
 daily_spec <- spectrum(daily_df$CO2 , span=5 , plot=F)
 
 plot_spectrograph <- function(fft_spec , period , x_label){
-fft_spec <- as.data.frame(cbind(fft_spec$freq , fft_spec$spec))
-names(fft_spec) <- c("freq" , "spec")
-
-fft_spec <- fft_spec %>%
-  filter(freq > 0.02)
-
-ggplot(fft_spec , aes(x=1/freq , y=log(spec)))+
-  geom_line()+
-  labs(title=paste("Smoothed Spectrogram of", period , "CO2 data"))+
-  xlab(paste0("period(" , x_label , ")"))+
-  ylab("spectral density")
+  fft_spec <- as.data.frame(cbind(fft_spec$freq , fft_spec$spec))
+  names(fft_spec) <- c("freq" , "spec")
+  
+  fft_spec <- fft_spec %>%
+    filter(freq > 0.02)
+  
+  ggplot(fft_spec , aes(x=1/freq , y=log(spec)))+
+    geom_line()+
+    labs(title=paste("Smoothed Spectrogram of", period , "CO2 data"))+
+    xlab(paste0("period(" , x_label , ")"))+
+    ylab("spectral density")
 }
 
 p1<-plot_spectrograph(minute_spec , "raw","minutes")
@@ -78,11 +82,11 @@ p3<-plot_spectrograph(daily_spec , "daily","days")
 #ggplotly(p1,p2,p3)
 
 
-#plot outliers (ARIMA)
+#ARIMA to find outliers
 #####
 
-plot_outliers <- function(DF){
-  daily_ts <- ts(DF$CO2)
+
+  daily_ts <- ts(daily_df$CO2)
   minute.outlier <- tso(y=daily_ts,types=c("AO","LS","TC"))
   #plot(minute.outlier)
 
@@ -99,10 +103,133 @@ plot_outliers <- function(DF){
     geom_line(aes(x=x , y=adjusted) , col="grey")+
     geom_point(data=outlier_points , aes(x=x, y=actual) , col="red")+
     labs(title="daily CO2 concentration with outliers shown")
-  
-}
 
 
-#plot_outliers(df)
-#plot_outliers(hourly_df)
-plot_outliers(daily_df)
+
+#plot with plotly
+#####
+#need to make a single df
+daily_df$indx <- daily_df$indx *(60*24)
+hourly_df$indx <- hourly_df$indx *(60)
+df$indx <- df$time
+
+daily_df <-daily_df%>%
+  rename(daily_CO2 = CO2)
+
+hourly_df <-hourly_df%>%
+  rename(hourly_CO2 = CO2)
+
+all_data <- left_join(df , hourly_df , by = "indx")
+all_data <- left_join(all_data , daily_df , by="indx")%>%
+  select(time,CO2,hourly_CO2 , daily_CO2)
+
+all_data <- na.locf(all_data)
+all_data[is.na(all_data)] <- 0
+all_data <- all_data [-c(1:1500),]
+
+
+
+# font style
+f <- list(
+  family = "Courier New, monospace",
+  size = 18,
+  color = "black")
+
+
+#plot concentration
+dataplot <- plot_ly(data=all_data , x=~time)%>%
+  add_lines(y=~CO2 , name = "by-minute",visible=T)%>%
+  add_lines( y=~hourly_CO2 , name="by-hour",visible=F)%>%
+  add_lines( y=~daily_CO2 , name="by-day",visible=F)%>%
+  layout(
+    annotations=list(
+    text= "CO2 concentration",
+    font = f,
+    xref = "paper",
+    yref = "paper",
+    yanchor = "bottom",
+    xanchor = "center",
+    align = "center",
+    x = 0.5,
+    y = 1,
+    showarrow = FALSE),
+    xaxis = list(domain=c(0,43199)),
+    yaxis= list(title = 'Concentration (ppm)'),
+     updatemenus = list(
+ 
+       list(
+         y=0.7,
+         buttons=list(
+           list(method = "restyle",
+               args = list("visible", list(T,F,F)),
+                label = "by-minute"),
+           
+           list(method = "restyle",
+                args = list("visible", list(F,T,F)),
+                label = "by-hour"),
+         
+         list(method = "restyle",
+             args = list("visible", list(F,F,T)),
+              label = "by-day"))
+       )
+     )
+  )
+
+
+#plot FFT
+#make single df
+minute_fft <- data.frame(period = (1/minute_spec$freq) , minute_spec=log(minute_spec$spec))%>%
+  filter(period>1 , period < 50)
+
+hourly_fft <- data.frame(period = (1/hourly_spec$freq) , hourly_spec=log(hourly_spec$spec)) %>%
+  filter(period>1 , period < 50)
+
+daily_fft <- data.frame(period = (1/daily_spec$freq) , daily_spec=log(daily_spec$spec)) %>%
+  filter(period>1 , period < 50)
+
+fft_plot <- plot_ly(data=minute_fft , x=~period)%>%
+  add_lines(data = minute_fft , y=~minute_spec , name = "by-minute",visible=T)%>%
+  add_lines(data=hourly_fft , y=~hourly_spec , name="by-hour",visible=F)%>%
+  add_lines( data = daily_fft , y=~daily_spec , name="by-day",visible=F)%>%
+  layout(
+    annotations=list(
+    text= "Spectral plot of data",
+    font = f,
+    xref = "paper",
+    yref = "paper",
+    yanchor = "bottom",
+    xanchor = "center",
+    align = "center",
+    x = 0.5,
+    y = 1,
+    showarrow = FALSE),
+    xaxis = list(domain=c(0,50)),
+    yaxis= list(title = 'Concentration (ppm)'),
+    updatemenus = list(
+      
+      list(
+        y=0.7,
+        buttons=list(
+          list(method = "restyle",
+               args = list("visible", list(T,F,F)),
+               label = "by-minute"),
+          
+          list(method = "restyle",
+               args = list("visible", list(F,T,F)),
+               label = "by-hour"),
+          
+          list(method = "restyle",
+               args = list("visible", list(F,F,T)),
+               label = "by-day"))
+      )
+    )
+  )
+
+
+#plot outliers
+# outlier_plot <- plot_ly(data=outlier_df , x=~period)%>%
+#   add_lines(data = minute_fft , y=~minute_spec , name = "by-minute",visible=T)%>%
+#   add_lines(data=hourly_fft , y=~hourly_spec , name="by-hour",visible=F)%>%
+# 
+# subplot(dataplot , fft_plot , nrows=2 , titleX=T , titleY=T ,margin=0.1 )
+
